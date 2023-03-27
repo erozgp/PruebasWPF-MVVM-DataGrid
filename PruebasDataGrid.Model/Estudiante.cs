b6cbc5f3244e78dbf21eb0e8dd39c4e9b6b9ca9c
Erick <erozgp@gmail.com>
2023-03-27 16:25:19 -0600

    Agregar archivos de proyecto.

---
+++ PruebasDataGrid.Model/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasDataGrid.Model
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Email { get; set; }
+
+        public Estudiante(string name, int age, string email)
+        {
+            Nombre = name;
+            Edad = age;
+            Email = email;
+        }
+    }
+}
---
+++ PruebasDataGrid.ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using PruebasDataGrid.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasDataGrid.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private List<Estudiante> estudiantesList;
+       
+
+
+        public List<Estudiante> EstudiantesList
+        {
+            get { return estudiantesList; }
+
+            set
+            {
+                if (estudiantesList == value)
+                {
+                    return;
+                }
+                estudiantesList = value;
+                OnPropertyChanged("EstudiantesList");
+            }
+        }
+
+        public MainViewModel()
+        {
+            List<Estudiante> estudianteList = new List<Estudiante>();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                estudianteList.Add(new Estudiante("Estudiante"+i, 23+i, "erozgp"+i+"@gmail.com"));
+            }
+                    
+            EstudiantesList = estudianteList;            
+        }
+    }
+}
